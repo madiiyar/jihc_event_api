1124def56c59a63782339a0f7a9ded442f306bfb
Madiyar <amadiar654@gmail.com>
2024-12-20 01:29:15 +0500

    updated cors

--- jihc_event_api/Program.cs
+++ jihc_event_api/Program.cs
@@ -4,8 +4,21 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<JihcEventsContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("defcon")));
+builder.Services.AddDbContext<JihcEventsContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("defcon")));
 builder.Services.AddControllers();
+
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,6 +31,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Enable CORS
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
